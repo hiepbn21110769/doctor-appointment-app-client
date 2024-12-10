243e67ef264249524c8b3a41231842fce3d01b37
hiepbui32 <21110769@student.hcmute.edu.vn>
2024-12-11 00:30:48 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,2 @@
-export const host = 'http://localhost:8080';
+export const host = 'https://a3eb-2402-800-6327-1bd5-55bb-a030-c8a0-6d22.ngrok-free.app';
 export const feAPI = 'http://localhost:4200';
